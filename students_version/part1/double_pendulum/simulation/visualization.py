d08ae2f2b361d5aa1ef98c4f0f0a2903c7aeb5d2
Raphael Stöckner <stockner@kth.se>
2026-04-27 10:19:08 +0200

    fixed matplotlib error

--- students_version/part1/double_pendulum/simulation/visualization.py
+++ students_version/part1/double_pendulum/simulation/visualization.py
@@ -20,7 +20,7 @@ def get_arrow(radius, centX, centY, angle_, theta2_, color_='black'):
 
     head = RegularPolygon((endX, endY),            # (x,y)
                           3,                       # number of vertices
-                          radius/20,                # radius
+                          radius=radius/20,                # radius
                           rad(angle_+theta2_),     # orientation
                           color=color_)
     return arc, head
